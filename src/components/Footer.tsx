c05b3cf6ab72236593ac80a6aeeb3c8181698623
youngkwang.song <youngkwang.song@lgcns.com>
2022-12-01 17:33:11 +0900

    feat: Footer MUI 로의 전환

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { HomeRounded, ManageSearchRounded, PeopleRounded, SettingsRounded } from '@mui/icons-material';
+
+const useStyles = makeStyles({
+  navigation: {
+    fontFamily: 'Noto Sans KR',
+  },
+  action: {
+    backgroundColor: 'black',
+  },
+});
+
+// https://stackoverflow.com/questions/54375096/styling-bottomnavigation-in-react-js-material-ui
 
 const Footer = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
   return (
-    <footer className="Footer">
-      <div className="Footer_item">메인 화면</div>
-      <div className="Footer_item">모임 추가</div>
-      <div className="Footer_item">모임 관리</div>
-      <div className="Footer_item">설정</div>
-    </footer>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.navigation}
+    >
+      <BottomNavigationAction className={classes.action} label="Home" icon={<HomeRounded />} />
+      <BottomNavigationAction className={classes.action} label="모임 탐색" icon={<ManageSearchRounded />} />
+      <BottomNavigationAction className={classes.action} label="모임 관리" icon={<PeopleRounded />} />
+      <BottomNavigationAction className={classes.action} label="계정" icon={<SettingsRounded />} />
+    </BottomNavigation>
   );
 };
 
